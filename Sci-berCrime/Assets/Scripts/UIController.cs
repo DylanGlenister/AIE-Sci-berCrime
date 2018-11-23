2d1622436be576f64ec0bbd8c7f4376415dc8317
SlenderFox <36467674+SlenderFox@users.noreply.github.com>
2018-11-23 15:26:37 +1100

    -Added more functionality for UI

--- Sci-berCrime/Assets/Scripts/UIController.cs
+++ Sci-berCrime/Assets/Scripts/UIController.cs
@@ -28,17 +28,47 @@ public class UIController : MonoBehaviour
     public Text m_txtPlayerTwoShopHealth;
     public Text m_txtPlayerTwoShopAmmo;
 
-    //public Text m_txtHealthUpgradeCost;
-    //public Text m_txtDamageUpgradeCost;
-    //public Text m_txtRPMUpgradeCost;
-    //public Text m_txtAmmoUpgradeCost;
-    //public Text m_txtPiercingUpgradeCost;
-    //public Text m_txtSpreadUpgradeCost;
-    //public Text m_txtExplosiveUpgradeCost;
-    //public Text m_txtHealthBuyCost;
-    //public Text m_txtAmmoBuyCost;
+    [Header("Upgrade costs - Player One")]
+    public Text m_txtP1HealthUpgradeCost;
+    public Text m_txtP1DamageUpgradeCost;
+    public Text m_txtP1RPMUpgradeCost;
+    public Text m_txtP1AmmoUpgradeCost;
+    public Text m_txtP1PiercingUpgradeCost;
+    public Text m_txtP1SpreadUpgradeCost;
+    public Text m_txtP1ExplosiveUpgradeCost;
+    public Text m_txtP1HealthBuyCost;
+    public Text m_txtP1AmmoBuyCost;
 
-    private void Awake()
+    [Header("Upgrade costs - Player Two")]
+    public Text m_txtP2HealthUpgradeCost;
+    public Text m_txtP2DamageUpgradeCost;
+    public Text m_txtP2RPMUpgradeCost;
+    public Text m_txtP2AmmoUpgradeCost;
+    public Text m_txtP2PiercingUpgradeCost;
+    public Text m_txtP2SpreadUpgradeCost;
+    public Text m_txtP2ExplosiveUpgradeCost;
+    public Text m_txtP2HealthBuyCost;
+    public Text m_txtP2AmmoBuyCost;
+
+    [Header("Upgrade levels - Player One")]
+    public Text m_txtP1HealthUpgradeLevel;
+    public Text m_txtP1DamageUpgradeLevel;
+    public Text m_txtP1RPMUpgradeLevel;
+    public Text m_txtP1AmmoUpgradeLevel;
+    public Text m_txtP1PiercingUpgradeLevel;
+    public Text m_txtP1SpreadUpgradeLevel;
+    public Text m_txtP1ExplosiveUpgradeLevel;
+
+    [Header("Upgrade levels - Player Two")]
+    public Text m_txtP2HealthUpgradeLevel;
+    public Text m_txtP2DamageUpgradeLevel;
+    public Text m_txtP2RPMUpgradeLevel;
+    public Text m_txtP2AmmoUpgradeLevel;
+    public Text m_txtP2PiercingUpgradeLevel;
+    public Text m_txtP2SpreadUpgradeLevel;
+    public Text m_txtP2ExplosiveUpgradeLevel;
+
+    private void Awake ()
     {
         m_txtRoundTimer.enabled = false;
         m_goShopWindow.SetActive(false);
@@ -138,5 +168,118 @@ public class UIController : MonoBehaviour
     public void SetPlayerTwoShopAmmo (int pPlayerTwoAmmo)
     {
         m_txtPlayerTwoShopAmmo.text = pPlayerTwoAmmo.ToString();
+    }
+
+    //----------Costs----------
+    // Player one
+
+    // Updates the cost for the health upgrade of player ones
+    public void UpdatePlayerOneHealthUpgradeCost (int pValue)
+    {
+        m_txtP1HealthUpgradeCost.text = pValue.ToString();
+    }
+
+    // Updates the cost for the damage upgrade of player ones
+    public void UpdatePlayerOneDamageUpgradeCost (int pValue)
+    {
+        m_txtP1DamageUpgradeCost.text = pValue.ToString();
+    }
+
+    // Updates the cost for the RPM upgrade of player ones
+    public void UpdatePlayerOneRPMUpgradeCost (int pValue)
+    {
+        m_txtP1RPMUpgradeCost.text = pValue.ToString();
+    }
+    
+    // Updates the cost for the ammo upgrade of player ones
+    public void UpdatePlayerOneAmmoUpgradeCost(int pValue)
+    {
+        m_txtP1AmmoUpgradeCost.text = pValue.ToString();
+    }
+
+    // Updates the cost for the spread upgrade of player ones
+    public void UpdatePlayerOneSpreadUpgradeCost (int pValue)
+    {
+        m_txtP1SpreadUpgradeCost.text = pValue.ToString();
+    }
+
+    // Updates the cost for the piercing upgrade of player ones
+    public void UpdatePlayerOnePiercingUpgradeCost (int pValue)
+    {
+        m_txtP1PiercingUpgradeCost.text = pValue.ToString();
+    }
+
+    // Updates the cost for the explosive upgrade of player ones
+    public void UpdatePlayerOneExplosiveUpgradeCost (int pValue)
+    {
+        m_txtP1ExplosiveUpgradeCost.text = pValue.ToString();
+    }
+
+    // Updates the cost for the health buy of player ones
+    public void UpdatePlayerOneHealthBuyCost (int pValue)
+    {
+        m_txtP1HealthBuyCost.text = pValue.ToString();
+    }
+
+    // Updates the cost for the ammo buy of player ones
+    public void UpdatePlayerOneAmmoBuyCost (int pValue)
+    {
+        m_txtP1AmmoBuyCost.text = pValue.ToString();
+    }
+
+    // Player two
+
+    // Updates the cost for the health upgrade of player twos
+    public void UpdatePlayerTwoHealthUpgradeCost (int pValue)
+    {
+        m_txtP2HealthUpgradeCost.text = pValue.ToString();
+    }
+
+    // Updates the cost for the damage upgrade of player twos
+    public void UpdatePlayerTwoDamageUpgradeCost (int pValue)
+    {
+        m_txtP2DamageUpgradeCost.text = pValue.ToString();
+    }
+
+    // Updates the cost for the RPM upgrade of player twos
+    public void UpdatePlayerTwoRPMUpgradeCost (int pValue)
+    {
+        m_txtP2RPMUpgradeCost.text = pValue.ToString();
+    }
+
+    // Updates the cost for the ammo upgrade of player twos
+    public void UpdatePlayerTwoAmmoUpgradeCost (int pValue)
+    {
+        m_txtP2AmmoUpgradeCost.text = pValue.ToString();
+    }
+
+    // Updates the cost for the spread upgrade of player twos
+    public void UpdatePlayerTwoSpreadUpgradeCost (int pValue)
+    {
+        m_txtP2SpreadUpgradeCost.text = pValue.ToString();
+    }
+
+    // Updates the cost for the piercing upgrade of player twos
+    public void UpdatePlayerTwoPiercingUpgradeCost (int pValue)
+    {
+        m_txtP2PiercingUpgradeCost.text = pValue.ToString();
+    }
+
+    // Updates the cost for the explosive upgrade of player twos
+    public void UpdatePlayerTwoExplosiveUpgradeCost (int pValue)
+    {
+        m_txtP2ExplosiveUpgradeCost.text = pValue.ToString();
+    }
+
+    // Updates the cost for the health buy of player twos
+    public void UpdatePlayerTwoHealthBuyCost (int pValue)
+    {
+        m_txtP2HealthBuyCost.text = pValue.ToString();
+    }
+
+    // Updates the cost for the ammo buy of player twos
+    public void UpdatePlayerTwoAmmoBuyCost (int pValue)
+    {
+        m_txtP2AmmoBuyCost.text = pValue.ToString();
     }
 }
